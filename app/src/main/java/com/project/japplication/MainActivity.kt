70d8ece8390570ae189c9472997fbf2768fbbbcf
RemiDK <remi_kergo@hotmail.com>
2020-08-13 12:03:26 +0200

    link between main and JLPT activity

--- app/src/main/java/com/project/japplication/MainActivity.kt
+++ app/src/main/java/com/project/japplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.project.japplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.project.japplication.jlpt.Jlpt5
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jlptButton = findViewById<Button>(R.id.jlptButton)
+        jlptButton?.setOnClickListener {
+            val intent = Intent(this, Jlpt5::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/project/japplication/jlpt/jlpt5.kt
@@ -0,0 +1,12 @@
+package com.project.japplication.jlpt
+
+import android.app.Activity
+import android.os.Bundle
+import com.project.japplication.R
+
+class Jlpt5 : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jlpt5)
+    }
+}
