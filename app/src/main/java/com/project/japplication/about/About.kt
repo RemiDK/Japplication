1561a2f8098792ddd09fb40f686abdb83683df48
RemiDK <remi_kergo@hotmail.com>
2020-08-14 14:52:51 +0200

    custom action bar added

--- app/src/main/java/com/project/japplication/about/About.kt
+++ app/src/main/java/com/project/japplication/about/About.kt
@@ -2,11 +2,18 @@ package com.project.japplication.about
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
 import com.project.japplication.R
 
 class About : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about)
+        title="A propos"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
--- app/src/main/java/com/project/japplication/jlpt/Jlpt5.kt
+++ app/src/main/java/com/project/japplication/jlpt/Jlpt5.kt
@@ -2,11 +2,18 @@ package com.project.japplication.jlpt
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Menu
 import com.project.japplication.R
 
 class Jlpt5 : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.jlpt5)
+        title = "JLPT"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
